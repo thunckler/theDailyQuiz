6324365a06f81f26c828501f79a155cb332d2eab
thunckler <thunckler@twilio.com>
2017-04-25 15:03:27 -0700

    Updated send request
    Update

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
 var http = require('http'),
     express = require('express'),
     twilio = require('twilio'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    Sequelize = require('sequelize');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-//
-//var getQID = squel.select()
-//    .from('NumberLog')
-//    .field(QID)
-//    .where('phoneNumber', userId);
-
-//var getQuestion = squel.select()
-//    .from('Questions')
-//    .field(answer)
-//    .where('QID', userId);
-//
-
-app.get('/', function(req, res) {
-    var twilio = require('twilio');
-    var twiml = new twilio.TwimlResponse();
-    if (req.query.Body == 'hello') {
-        twiml.message('Hi!');
-    } else if(req.query.Body == 'bye') {
-        twiml.message('Goodbye');
-    } else {
-        twiml.message('No Body param match, Twilio sends this in the request to your server.');
-    }
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-});
+const models = require('./models')
 
 app.post('/', function(req, res) {
     var twilio = require('twilio');
     var twiml = new twilio.TwimlResponse();
-    if (req.body.Body == 'hello') {
-        twiml.message('Hi!');
-    } else if(req.body.Body == 'bye') {
-        twiml.message('Goodbye');
-    } else {
-        twiml.message('No Body param match, Twilio sends this in the request to your server.');
-    }
+    
+    models.Question.find({
+    order: [
+      Sequelize.fn( 'RANDOM' ),
+    ]
+  }).then(function(question) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.message("Your Question is:\n" + question.question)
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
+  })
 });
 
 http.createServer(app).listen(1337, function () {
     console.log("Express server listening on port 1337");
 });
+
